2b06a21cad784bbcb62716647b1ebae2be1035d5
jamesthenpc <jamesthomashaggerty@gmail.com>
2017-07-11 12:39:39 -0400

    made changes to the nav bar

--- app/controllers/static/_pages_controller.rb
+++
@@ -1,7 +0,0 @@
-class Static::PagesController < ApplicationController
-  def about
-  end
-
-  def contact
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
   get 'static_pages/about'
-
   get 'static_pages/contact'
-
-  namespace :static do
-    get '_pages/about'
-  end
-
-  namespace :static do
-    get '_pages/contact'
-  end
-
   get 'static_pages/index'
   root 'static_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
