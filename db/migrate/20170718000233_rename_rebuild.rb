fc3bafe8b2fb221b1fe84e79c7da028446565b75
jamesthenpc <jamesthomashaggerty@gmail.com>
2017-07-18 18:47:31 -0400

    revamed

--- db/migrate/20170718000233_rename_rebuild.rb
+++
@@ -1,5 +0,0 @@
-class RenameRebuild < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :products, :colour
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170718000233) do
+ActiveRecord::Schema.define(version: 20170718223143) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170718000233) do
     t.datetime "updated_at", null: false
     t.string "colour"
     t.string "string"
+    t.string "color"
   end
 
 end
