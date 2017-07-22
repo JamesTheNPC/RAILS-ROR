61bf2b0ae7aa7471a21193ebc80e3859b913a96a
jamesthenpc <jamesthomashaggerty@gmail.com>
2017-07-22 11:09:31 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!")
-Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!")
-Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!")
-Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!")
-Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!")
-Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!")
+Product.create(name: "Arkham Horror", description: "Board game" price_in_dollars: "22.0" )
+Product.create(name: "Mythos Tales", description: "Investigation board game" price_in_dollars: "12.0")
+Product.create(name: "Call of Cthulu" description: "RPG board game" price_in_dollars: '6.0"')
+Product.create(name: "Mansions of Madness" description: "Audio board game", price_in_dollars: "5.0")
+
 
 
 
