25727a83df7eec018aa76db1a602a5b9f9df8817
jamesthenpc <jamesthomashaggerty@gmail.com>
2017-07-22 11:19:22 -0400

    still seedy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(name: "Arkham Horror", description: "Board game" price_in_dollars: "22.0" )
-Product.create(name: "Mythos Tales", description: "Investigation board game" price_in_dollars: "12.0")
-Product.create(name: "Call of Cthulu", description: "RPG board game" price_in_dollars: '6.0"')
-Product.create(name: "Mansions of Madness", description: "Audio board game", price_in_dollars: "5.0")
+Product.create(name: "Arkham Horror", description: "Board game", price: "22.0" )
+Product.create(name: "Mythos Tales", description: "Investigation board game", price: "12.0")
+Product.create(name: "Call of Cthulu", description: "RPG board game", price: "6.0")
+Product.create(name: "Mansions of Madness", description: "Audio board game", price: "5.0")
 
 
 
