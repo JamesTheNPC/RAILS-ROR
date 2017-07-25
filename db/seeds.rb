91e71a98c5a9ecf9dd6d1693a5ed48fae8c281cf
jamesthenpc <jamesthomashaggerty@gmail.com>
2017-07-24 21:18:47 -0400

    destoy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.destroy_all()
+Product.destroy_all 
 Product.create(name: "Arkham Horror", description: "Board game", image_url: "https://i.imgur.com/xK1rHDZ.jpg", price: "22.0" )
 Product.create(name: "The Mythos Tales", description: "Investigation board game", image_url: "https://i.imgur.com/cruqS0J.jpg", price: "12.0")
 Product.create(name: "Eldritch Horror", description: "RPG board game", image_url: "https://i.imgur.com/4sx2tZV.jpg", price: "6.0")
 Product.create(name: "Mansions of Madness 2.0", description: "Audio board game", image_url: "https://i.imgur.com/AiNcYRn.jpg", price: "5.0")
-
-
-
 
